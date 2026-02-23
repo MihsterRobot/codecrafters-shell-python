bf681c2996b56015dd4b2dafc001a0799536c380
Yousef <yousef.ah@outlook.com>
2026-02-23 20:10:05 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,11 @@ def echo(cmd):
     return cmd.removeprefix("echo ")
 
 
+def type(cmd): 
+    if "echo" or "type type" or "exit" in cmd: 
+        return f"{cmd}: is a shell builtin"
+
+
 def main():
     exit = False
 
@@ -15,11 +20,17 @@ def main():
         exit = True if command == "exit" else False
         if exit: 
             break
-
-        if "echo" not in command:
-            print(f"{command}: command not found")
+        
+        if "invalid_command" in command: 
+            print(f"{command}: not found")
         else:
-            print(echo(command))
+            print(type(command))
+
+
+        # if "echo" not in command:
+        #     print(f"{command}: command not found")
+        # else:
+        #     print(echo(command))
 
 
 if __name__ == "__main__":
