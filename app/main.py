f2642e0b19936e706fe231c412b17c1ef9317da2
Yousef <yousef.ah@outlook.com>
2026-02-23 19:40:05 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,9 +2,6 @@ import sys
 
 
 def main():
-    # Map command names to their functionality
-    commands = {"exit": "exit", "echo": echo}
-
     while True:
         sys.stdout.write("$ ")
         command = input()
