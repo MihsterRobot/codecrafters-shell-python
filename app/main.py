0e262d83f175f7e8b711152db926822c4d5f4a22
Yousef <yousef.ah@outlook.com>
2026-02-23 17:04:38 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,16 +6,20 @@ def echo(cmd):
 
 
 def main():
+    # Map command names to their functionality
     commands = {"exit": "exit", "echo": echo}
 
     while True:
         sys.stdout.write("$ ")
         command = input()
 
-        result = commands.get(command)
-        if result == "exit": 
+        # Grab the command 
+        handler = commands.get(command)
+        if handler == "exit" or handler == None: 
             break
         
+        # Run echo() 
+        result = handler(command)
         print(result)
 
         # print(f"{command}: command not found")
