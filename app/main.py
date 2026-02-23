1ac786b5e746c5d85ffbc0c9e6a36c6880a4661e
Yousef <yousef.ah@outlook.com>
2026-02-23 19:39:23 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 import sys
 
 
-def echo(cmd):
-    return cmd.removeprefix("echo")
-
-
 def main():
     # Map command names to their functionality
     commands = {"exit": "exit", "echo": echo}
@@ -13,16 +9,7 @@ def main():
         sys.stdout.write("$ ")
         command = input()
 
-        # Grab the command 
-        handler = commands.get(command)
-        if handler == "exit" or handler == None: 
-            break
-        print("This is the user's command " + command)
-        # Run echo() 
-        result = handler(command)
-        print(result)
-
-        # print(f"{command}: command not found")
+        print(command.removeprefix("echo"))
 
 
 if __name__ == "__main__":
