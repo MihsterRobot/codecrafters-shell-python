a011ba9bd9f80dbbb3ac8f14ae039823765b2925
Yousef <yousef.ah@outlook.com>
2026-02-23 16:26:27 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ import sys
 
 
 def main():
-    terminated = False
-
-    while not terminated: 
+    while True:
         sys.stdout.write("$ ")
         command = input()
+
+        if command == "exit": 
+            break
+
         print(f"{command}: command not found")
 
-        
+
 if __name__ == "__main__":
     main()
