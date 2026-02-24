3d11d6a57d327ab37107e982859083be131a05fb
Yousef <yousef.ah@outlook.com>
2026-02-24 14:50:24 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,18 +2,18 @@ import sys
 
 
 def run_echo(cmd): 
-    print(cmd)
+    return cmd
 
 
 def run_type(cmd): 
     if cmd in ("echo", "type", "exit"): 
-        print(f"{cmd} is a shell builtin")
+        return f"{cmd} is a shell builtin"
     else: 
-        print(f"{cmd}: not found")
+        return f"{cmd}: not found"
 
 
 def run_exit(cmd):
-    return False
+    return "EXIT"
 
 
 COMMANDS = {"echo": run_echo, "type": run_type, "exit": run_exit}
@@ -40,8 +40,13 @@ def main():
         # Grab the command's arguments 
         command_args = command.removeprefix(cmd + " ")
 
-        # Execute the command
-        handler(command_args)
+        # Execute the command and store the return value
+        result = handler(command_args)
+
+        if result == "EXIT":
+            break
+        else: 
+            print(result)
 
 
 if __name__ == "__main__":
