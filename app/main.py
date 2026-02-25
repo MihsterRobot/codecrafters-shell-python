fe24deecd62c2a3998bb8c24fcf52e5b0eeb743b
Yousef <yousef.ah@outlook.com>
2026-02-25 05:14:34 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,26 +6,26 @@ from .commands import COMMANDS, EXIT
 def main():
     while True:
         command = input("$ ").split()
+        print(command)
         
         # Extract the command name (first token) from the user's input
         command_name = command[0]
+        
+        if command_name in COMMANDS: 
+            # Retrieve the command's handler and isolate the raw argument string
+            handler = COMMANDS[command_name] 
+            command_args = command.removeprefix(command_name)
 
+            output, signal = handler(command_args)
+
+            if signal is EXIT:
+                break
+
+            if output is not None: 
+                print(output)
+        
         # Command name isn't a builtin or registered handler
-        if command_name not in COMMANDS: 
-            print(f"{command_name}: not found")
-            continue
-
-        # Retrieve the command's handler and isolate the raw argument string
-        handler = COMMANDS[command_name] 
-        command_args = command.removeprefix(command_name + " ")
-
-        output, signal = handler(command_args)
-
-        if signal is EXIT:
-            break
-
-        if output is not None: 
-            print(output)
+        print(f"{command_name}: not found")
 
 
 if __name__ == "__main__":
