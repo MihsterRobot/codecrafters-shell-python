f62d75ee522091d8fb1f32043df85413e3650996
Yousef <yousef.ah@outlook.com>
2026-03-01 08:49:38 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ def main():
         if path is not None: 
             arg_list = shlex.split(raw_args)
 
-            print(c.run_external_program(path, raw_args), end="")
+            print(c.run_external_program(path, arg_list), end="")
 
             continue
         
