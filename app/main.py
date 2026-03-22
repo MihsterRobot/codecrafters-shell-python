8dab1b8e83efaa7d8dae1b57d5cf8dd49d732c2a
Yousef <yousef.ah@outlook.com>
2026-03-22 08:19:35 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ def main():
                 break
 
             # Redirect stdout to file if specified, otherwise print to terminal
+            # '\n' is added explicitly since f.write() does not append a newline unlike print()
             if stdout and stdout_file_path: 
                     with open(stdout_file_path, stdout_mode) as f:
                         f.write(stdout + '\n')
