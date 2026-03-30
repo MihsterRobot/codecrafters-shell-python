bbcabb94a19d992d371f2cde628a1da59c43c787
Yousef <yousef.ah@outlook.com>
2026-03-30 11:35:16 +0100

    codecrafters submit [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -366,7 +366,7 @@ def get_directory_completions(text):
         if os.path.isdir(directory):
             full_path = os.path.join(os.getcwd(), directory)
             for name in os.listdir(full_path):
-                if os.path.isfile(os.path.join(directory, name)) and name.startswith(prefix):
+                if os.path.isdir(os.path.join(directory, name)) and name.startswith(prefix):
                     matches.append(os.path.join(directory, name))
     else:
         for name in os.listdir(os.getcwd()):
