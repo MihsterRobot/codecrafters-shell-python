a595005648da39ea76aa129b55ff2f5c200c40d9
Yousef <yousef.ah@outlook.com>
2026-05-06 09:10:08 +0100

    feat: implement completion registration and display [skip ci]

--- app/commands.py
+++ app/commands.py
@@ -657,13 +657,21 @@ def get_path_completions(text: str, entry_type: Literal['file', 'dir']) -> list[
 
 def run_complete(args: str) -> tuple[str | None, None]:
     '''Display or manage tab completion specifications for commands.'''
-    if '-p' in args:
-        arg = args.split()[1]
-        return f'complete: {arg}: no completion specification' + '\n', None
+    cmd = args.split()[-1]
 
+    if '-C' in args:
+        spec = args.split()[1]
+        completion_specs[cmd] = spec
+    elif '-p' in args:
+        spec = completion_specs.get(cmd)
+        if spec is None:
+            return f'complete: {cmd}: no completion specification\n', None
+        return f'complete -C \'{spec}\' {cmd}\n', None
+    
     return None, None
 
 
+completion_specs = dict()
 history = HistoryState()
 job_state = JobState()
 
